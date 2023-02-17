3a3ff2f52cf8812e4350b7f5d07a6026dffa47ef
Jinyoung Jang <jyjang@uengine.org>
2023-02-18 01:44:48 +0900

    commit

---
+++ monolith/src/main/java/labshop/domain/Order.java
@@ -0,0 +1,43 @@
+package labshop.domain;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import labshop.MonolithApplication;
+import labshop.domain.OrderPlaced;
+import lombok.Data;
+import org.springframework.context.ApplicationContext;
+
+@Entity
+@Table(name = "Order_table")
+@Data
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String productId;
+
+    private Integer qty;
+
+    private String customerId;
+
+    private Double amount;
+
+    @PostPersist
+    public void onPostPersist() {
+        OrderPlaced orderPlaced = new OrderPlaced(this);
+        orderPlaced.publishAfterCommit();
+    }
+
+    public static OrderRepository repository() {
+        OrderRepository orderRepository = applicationContext()
+            .getBean(OrderRepository.class);
+        return orderRepository;
+    }
+
+    public static ApplicationContext applicationContext() {
+        return MonolithApplication.applicationContext;
+    }
+}
