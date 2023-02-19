e313836182b3a6f8d087d25d2a3a93110c169abf
Jinyoung Jang <jyjang@uengine.org>
2023-02-19 10:42:26 +0000

    s

--- order/src/main/java/labshop/domain/OrderDetail.java
+++ order/src/main/java/labshop/domain/OrderDetail.java
@@ -4,7 +4,11 @@ import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import org.springframework.beans.BeanUtils;
 
 @Embeddable
