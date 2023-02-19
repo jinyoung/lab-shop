578fa2c2386273c2b4d284396194fcdc15a54bea
Jinyoung Jang <jyjang@uengine.org>
2023-02-19 19:36:37 +0900

    commit

---
+++ order/src/main/java/labshop/domain/OrderDetail.java
@@ -0,0 +1,21 @@
+package labshop.domain;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+import org.springframework.beans.BeanUtils;
+
+@Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDetail {
+
+    private String productId;
+
+    private Long qty;
+
+    private BigDecimal price;
+}
